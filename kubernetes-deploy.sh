3c4accb68444a6671200906c53f48866d05f911a
Jiayin Zhuo <jiayin.zhuo@gmail.com>
2021-08-17 12:28:21 -0400

    Update to match project id

--- kubernetes-deploy.sh
+++ kubernetes-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-kubectl create deployment productservice --image=jiayinzhuo/productservice
+kubectl create deployment productservice --image=jiayinzhuo/productservice:1.0
 kubectl expose deployment productservice --type="LoadBalancer" --port 8080
